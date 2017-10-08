54af3d12df6390a87e847ef9b1cb1b46e54b9065
loreanazic <loreana.zambrano@unet.edu.ve>
2017-10-08 04:37:37 -0400

    formulario nuevo

---
+++ js/JS.js
@@ -0,0 +1,39 @@
+    
+    document.getElementById("botonSiguienteEstado").addEventListener("click", mostrarEstado2);
+    document.getElementById("botonEstadoAnterior").addEventListener("click", mostrarEstado1);
+
+    function mostrarEstado2() {
+        document.getElementById("estado1").className = "animated bounceOutLeft";
+        setTimeout(function(){ 
+            document.getElementById("estado1").style.display = "none"; 
+            document.getElementById("estado2").style.display = "block";
+            document.getElementById("estado2").className = "animated bounceInRight";
+            
+            document.getElementById("elDiv1").className = "paso datos col-xs-12 col-sm-2 col-md-2 col-lg-2 col-md-offset-1";
+            document.getElementById("elDiv2").className = "paso obra active col-xs-12 col-sm-4 col-md-4 col-lg-4";
+        }, 1000);
+
+    }
+    
+    function mostrarEstado1() {
+        document.getElementById("estado2").className = "animated bounceOutLeft";
+        
+        setTimeout(function(){ 
+            document.getElementById("estado2").style.display = "none"; 
+            document.getElementById("estado1").style.display = "block";
+            document.getElementById("estado1").className = "animated bounceInRight";
+            
+            document.getElementById("elDiv1").className = "paso datos active col-xs-12 col-sm-2 col-md-2 col-lg-2 col-md-offset-1";
+            document.getElementById("elDiv2").className = "paso obra col-xs-12 col-sm-4 col-md-4 col-lg-4";
+        }, 1000);
+    }
+    
+    function animacion(id, clase) {
+        $(id).removeClass("animated "+clase).addClass(clase + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+          $(this).removeClass("animated "+clase);
+        });
+    };
+    document.getElementById('archivo').onchange = function () {
+      console.log(this.value);
+      document.getElementById('plano').innerHTML = document.getElementById('archivo').files[0].name;
+    }
